4b0d99270f569b9acd344819e5b9c9307d281c92
Curro Campuzano <campuzanocurro@gmail.com>
2022-10-31 15:15:13 +0100

    feat: query

--- src/query_bed.py
+++ src/query_bed.py
@@ -7,11 +7,18 @@ from bed import (
     read_bed_file, print_line, BedLine
 )
 
+def is_overlapping(x: tuple[int,int], y: tuple[int,int]) -> bool:
+    # Credits to Thomas Maillund :)
+    return max(x[0], y[0]) < min(x[1], y[1])
+
+def is_feature_in(feat: BedLine, start: int, end: int):
+    interval = (feat.chrom_start, feat.chrom_end)
+    return is_overlapping(interval, (start, end))
 
 def extract_region(features: list[BedLine],
                    start: int, end: int) -> list[BedLine]:
     """Extract region chrom[start:end] and write it to outfile."""
-    return []  # FIXME: We want the actual region, not an empty list!
+    return [feat for feat in features if is_feature_in(feat, start, end)]
 
 
 def main() -> None:
--- src/query_bed_test.py
+++ src/query_bed_test.py
@@ -2,5 +2,34 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_1984():
-    assert 2 + 2 == 4
+from bed import BedLine
+from query_bed import extract_region
+
+
+def test_happy_path():
+    x = [ BedLine("chr1", 2, 3, "foo"),  BedLine("chr1", 4, 5, "foo")]
+    assert extract_region(x, 1, 10) == x
+    assert extract_region(x, 2, 5) == x
+    x = y = [ BedLine("chr1", 4, 5, "foo"), BedLine("chr1", 2, 3, "foo")]
+    assert extract_region(x, 1, 10) == x
+    assert extract_region(x, 2, 5) == x
+
+def test_empty_chr():
+    x = []
+    assert extract_region(x, 1, 10) == x
+
+def test_empty_query():
+    x = [ BedLine("chr1", 2, 3, "foo"),  BedLine("chr1", 4, 5, "foo")]
+    assert extract_region(x, 20, 100) == []
+
+def test_query_wrong():
+    x = [ BedLine("chr1", 2, 3, "foo"),  BedLine("chr1", 4, 5, "foo")]
+    assert extract_region(x, 2, 2) == []
+
+def test_one_feature():
+    x = [ BedLine("chr1", 2, 3, "foo"),  BedLine("chr1", 4, 5, "foo")]
+    assert extract_region(x, 2, 4) == [ BedLine("chr1", 2, 3, "foo") ]
+
+def test_non_SNP():
+    x = [ BedLine("chr1", 2, 8, "foo"),  BedLine("chr1", 4, 7, "foo")]
+    assert extract_region(x, 3, 5) == x
